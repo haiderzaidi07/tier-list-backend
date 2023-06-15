87ac01220c10eff994c2e6368300f420ce7261ff
Haider Zaidi <100717066+haiderzaidi07@users.noreply.github.com>
2023-06-15 20:28:59 +0500

    changed cors origin to netlify

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://tier-list.netlify.app',
     credentials: true
 }))
 
