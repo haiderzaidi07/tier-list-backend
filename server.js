22b94c46aa0687b0340f5aaae330f83592c4d84a
haiderzaidi07 <smhaider072@gmail.com>
2023-06-15 13:32:38 +0500

    changed origin 2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 app.use(express.json())
 app.use(cors({
-    origin: 'https://colorful-lamb-pinafore.cyclic.app/',
+    origin: 'http://localhost:3000',
     credentials: true
 }))
 
